70ba411c3bc2ff263fa50af51e0d8003aba9159e
slstevens <sean.stevens.123@gmail.com>
2014-11-03 14:49:31 +0000

    Added destroy method

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -66,17 +66,17 @@ describe 'creating restaurants' do
 end
 
 
-describe ‘deleting restaurants’ do
+describe 'deleting restaurants' do
 
 	  before do
 	    Restaurant.create(:name => "KFC")
 	  end
 
 	  it "removes a restaurant when a user clicks a delete link" do
-	    visit '/'
+	    visit '/restaurants'
 	    click_link 'Delete KFC'
 	    expect(page).not_to have_content 'KFC'
-	    expect(page).to have_content 'Restaurants deleted successfully'
+	    expect(page).to have_content 'Restaurant deleted successfully'
 	  end
 
 end
